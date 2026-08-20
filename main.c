ea7545e3047800cbf7696e0ee6c268a45af83a97
donotcallmehejlsberg <124447345+donotcallmehejlsberg@users.noreply.github.com>
2026-08-20 22:38:28 +0200

    feat: add basic command parser

--- main.c
+++ main.c
@@ -2,11 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER_SIZE 32
+#define BUFFER_SIZE 256
+#define MAX_ARGUMENTS 32
 
 int main(void) {
-  char *buffer = calloc(BUFFER_SIZE, sizeof(char));
+  const char delimiters[] = " \t\n";
+  char *token;
+  char *buffer = NULL;
+  buffer = calloc(BUFFER_SIZE, sizeof(char));
+  if (buffer == NULL) {
+    fprintf(stderr, "Memory allocation failed\n");
+    return EXIT_FAILURE;
+  }
 
+  char *command_argv[MAX_ARGUMENTS + 1];
   while (1) {
     printf("my shell > ");
     fflush(stdout);
@@ -22,11 +31,34 @@ int main(void) {
       continue;
     }
 
-    printf("You entered: %s", buffer);
+    int argument_count = 0;
+    token = strtok(buffer, delimiters);
+    if (token == NULL) {
+      continue;
+    }
+
+    while (token != NULL && argument_count < MAX_ARGUMENTS) {
+      command_argv[argument_count] = token;
+      argument_count++;
+
+      token = strtok(NULL, delimiters);
+    }
+
+    if (token != NULL) {
+      fprintf(stderr, "Too many arguments\n");
+      continue;
+    }
+
+    command_argv[argument_count] = NULL;
+    for (int i = 0; i < argument_count; i++) {
+      printf("command_argv[%d] = \"%s\"\n", i, command_argv[i]);
+    }
+
+    printf("command_argv[%d] = NULL\n", argument_count);
   }
 
   free(buffer);
   buffer = NULL;
 
-  return 0;
+  return EXIT_SUCCESS;
 }
