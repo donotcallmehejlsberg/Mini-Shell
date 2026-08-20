ff6c7cf7105413c20275351f54ffeb41ea6d3e24
donotcallmehejlsberg <124447345+donotcallmehejlsberg@users.noreply.github.com>
2026-08-20 21:13:12 +0200

    feat: add basic shell loop

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 32
 
 int main(void) {
-  printf("Hi :)\n");
+  char *buffer = calloc(BUFFER_SIZE, sizeof(char));
+
+  while (1) {
+    printf("my shell > ");
+    fflush(stdout);
+    if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+      break;
+    }
+
+    if (strcmp(buffer, "quit\n") == 0) {
+      break;
+    }
+
+    if (strcmp(buffer, "\n") == 0) {
+      continue;
+    }
+
+    printf("You entered: %s", buffer);
+  }
+
+  free(buffer);
+  buffer = NULL;
+
   return 0;
 }
