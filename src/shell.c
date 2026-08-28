a6a30ed5ce07bf1503da378c06be5b95d667683a
donotcallmehejlsberg <124447345+donotcallmehejlsberg@users.noreply.github.com>
2026-08-28 17:03:45 +0200

    feat: add fg built-in command

--- src/shell.c
+++ src/shell.c
@@ -363,6 +363,41 @@ static int bg(Job *job) {
   job->state = RUNNING;
   return EXIT_SUCCESS;
 }
+static int fg(Job *job, pid_t shell_pgid) {
+  if (job == NULL) {
+    fprintf(stderr, "fg: job not found\n");
+    return EXIT_FAILURE;
+  }
+
+  if (tcsetpgrp(STDIN_FILENO, job->pgid) == -1) {
+    perror("tcsetpgrp");
+    return EXIT_FAILURE;
+  }
+
+  if (kill(-job->pgid, SIGCONT) == -1) {
+    perror("kill");
+    tcsetpgrp(STDIN_FILENO, shell_pgid);
+    return EXIT_FAILURE;
+  }
+
+  job->state = RUNNING;
+
+  JobState job_state = RUNNING;
+  int command_status = waitForChild(job->pgid, &job_state);
+
+  if (tcsetpgrp(STDIN_FILENO, shell_pgid) == -1) {
+    perror("tcsetpgrp");
+    return EXIT_FAILURE;
+  }
+
+  if (job_state == DONE) {
+    removeJob(job);
+  } else if (job_state == STOPPED) {
+    job->state = STOPPED;
+  }
+
+  return command_status;
+}
 
 static Job *findJobByPgid(pid_t pgid) {
   for (int i = 0; i < MAX_JOBS; i++) {
@@ -534,6 +569,18 @@ static int executeCommand(char **command_argv, bool is_background,
   if (strcmp(command_argv[0], "jobs") == 0) {
     printJobs();
     return EXIT_SUCCESS;
+  }
+
+  if (strcmp(command_argv[0], "fg") == 0) {
+    if (command_argv[1] == NULL) {
+      fprintf(stderr, "fg: missing job ID\n");
+      return EXIT_FAILURE;
+    }
+
+    int job_id = atoi(command_argv[1]);
+    Job *job = findJobById(job_id);
+
+    return fg(job, shell_pgid);
   }
 
   if (strcmp(command_argv[0], "bg") == 0) {
