f292bfc7893389f47ae6b5062b3d783b39511d2a
donotcallmehejlsberg <124447345+donotcallmehejlsberg@users.noreply.github.com>
2026-08-31 12:21:24 +0200

    docs: document parser interface

--- include/parser.h
+++ include/parser.h
@@ -1,3 +1,8 @@
+/*
+ * Parses command input, expands environment variables,
+ * and detects shell operators such as &, redirections, and pipes.
+ */
+
 #ifndef MINI_SHELL_PARSER_H_
 #define MINI_SHELL_PARSER_H_
 
@@ -5,10 +10,19 @@
 
 #define MAX_ARGUMENTS 32
 
+/** Splits the input buffer into a NULL-terminated argument vector. */
 int tokenizeCommand(char *buffer, char **command_argv);
-bool isBackground(char **command_argv, int argument_count);
-int findRedirectionIndex(char **command_argv);
-int countPipes(char **command_argv);
+
+/** Replaces standalone "$NAME" arguments with environment values. */
 void expandEnvironment(char **command_argv);
+
+/** Checks whether the last argument is the "&" background operator. */
+bool isBackground(char **command_argv, int argument_count);
+
+/** Returns the index of the first <, >, or >> operator, or -1. */
+int findRedirectionIndex(char **command_argv);
+
+/** Returns the number of "|" operators in the argument vector. */
+int countPipes(char **command_argv);
 
 #endif
